a8659209e9d7095dcd041077b9776ee5f0e01b38
IRcodez <94713994+IRcodez@users.noreply.github.com>
2022-02-25 09:43:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,17 +24,39 @@ def die():
 	sprint(colored('You kicked the bucket', 'red'))
 	return start
 
-	print('''
+print('''
 
                
                                  
 		                         
 		     _______________________
-        |                       |
-	      | 	                    |
+                    |                       |
+	            | 	                    |
 		     \                     /
 		      \                   /
 		       \                 /
 		        \               /
 		         \             /
 		          \___________/''')
+
+def a():
+	sprint('You go to Walmart for some shopping')
+	print('|||SHOPPING LIST|||')
+	print('pringles')
+	print('rubber gloves')
+	print('bread')
+	print('soap')
+	print('milk')
+	print('lotion')
+	print('paper towels')
+	sleep(1)
+	print('1: Buy all')
+	print('2: Buy one at a time')
+	if a == '1':
+		sleep(1)
+		sprint('you bought all your groceries')
+	if a == '2':
+		sleep(1)
+		sprint('you buy all your groceries but at one at a time. The people of Dallas TX accuse you of being a witch for choosing such an option in a game where you can buy all at once.')
+		sleep(1)
+		die()
